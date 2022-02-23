a6b30c811f64e80633da5a2580da90ba29edb16e
harpia01 <andre23arruda@gmail.com>
2022-02-22 23:19:11 -0400

    feat: Update link

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const url = 'https://meet.google.com/getalink'
+    const url = 'https://meet.google.com/new'
     window.open(url, '_blank').focus()
 })
