768b0e197437a6ae9d1e9cc12fea3bc903e9788c
André Arruda <33252657+andre23arruda@users.noreply.github.com>
2022-08-04 10:34:35 -0400

    Update content.js

--- close_warning/content.js
+++ close_warning/content.js
@@ -1,11 +1,12 @@
 function modifyDOM() {
-    const query_list = ['#detecta-adblock']
+    const query_list = ['#detecta-adblock', '.barreiraJornada']
     for (query of query_list) {
         element = document.querySelector(query)
         if (element)
             element.style.setProperty('position', 'inherit')
     }
     document.body.style.setProperty('overflow', 'auto')
+    document.querySelector('html').style.setProperty('overflow', 'auto')
 }
 
 modifyDOM()
